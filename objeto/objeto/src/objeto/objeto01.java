bf40cdbab6bc64abd1f92ee1ffd79230c6e7a465
Mariá Dombrate <106116133+dombrate@users.noreply.github.com>
2022-07-17 09:23:33 -0300

    Add files via upload

---
+++ objeto/objeto/src/objeto/objeto01.java
@@ -0,0 +1,20 @@
+package objeto;
+
+public class objeto01 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		cliente cli = new cliente(); //criando o obj // estanciado o objeto
+		
+		cli.setNome("Adaberdaldo");
+		cli.setIdade(55);
+		cli.setTipoCliente("Ferramentas e Automotivo");
+		
+		System.out.println(cli.getNome());
+		System.out.println(cli.getIdade());
+		System.out.println(cli.getTipoCliente());
+	}
+
+}
